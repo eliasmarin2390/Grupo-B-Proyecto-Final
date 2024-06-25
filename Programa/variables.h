ca1efcd5cd4e68b7adc0eb97142682a72d2e14f9
eliasmarin2390 <165837117+eliasmarin2390@users.noreply.github.com>
2024-06-25 13:08:30 -0600

    update 25/06/2024

---
+++ Programa/variables.h
@@ -0,0 +1,55 @@
+#define MAX_REG 1000
+
+typedef struct ITEMS
+{
+    float extractoChocolate;
+    float extractoVainilla;
+    float harina;
+    float azucar;
+    float levadura;
+    float dulceLeche;
+    float lecheCondensada;
+    float fresa;
+    float pina;
+    float frutosRojos;
+    float leche;
+    float mantequilla;
+    float mora;
+    float cereza;
+    float mango;
+    float chispasChocolate;
+    int malvaviscos;
+    int huevos;
+    int empaques;
+    int id;
+    char fecha[30];
+};
+
+typedef struct ONGOINGITEMS
+{
+    float extractoChocolate;
+    float extractoVainilla;
+    float harina;
+    float azucar;
+    float levadura;
+    float dulceLeche;
+    float lecheCondensada;
+    float fresa;
+    float pina;
+    float frutosRojos;
+    float leche;
+    float mantequilla;
+    float mora;
+    float cereza;
+    float mango;
+    float chispasChocolate;
+    int malvaviscos;
+    int huevos;
+    int empaques;
+};
+
+ONGOINGITEMS curitems;
+ITEMS items[MAX_REG];
+int pos = 0;
+float tamano = 0;
+int cantidad = 0;
